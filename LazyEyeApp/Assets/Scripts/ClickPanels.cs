0325b57cb427a04decd7ed56f23888ceb7b40463
Spark11 <patlajanec@gmail.com>
2016-06-17 04:11:54 +0100

    Added 8 animated sprites

--- LazyEyeApp/Assets/Scripts/ClickPanels.cs
+++ LazyEyeApp/Assets/Scripts/ClickPanels.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class ClickPanels : MonoBehaviour {
 
+    private string spriteName = "knight";
+
     public Text uiText;
 
     private int correctPanel;
@@ -35,5 +37,35 @@ public class ClickPanels : MonoBehaviour {
         transform.GetChild(correctPanel).GetComponent<PanelSelected>().ToggleIsCorrect();       //  reset currently correct panel        
         correctPanel = Random.Range(0, 4);              //  choose new random correct panel        
         transform.GetChild(correctPanel).GetComponent<PanelSelected>().ToggleIsCorrect();       //  set the new chosen panel as correct      
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            spriteName = "knight";
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            spriteName = "dragon";
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+            spriteName = "princess";
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+            spriteName = "alien";
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+            spriteName = "fox";
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+            spriteName = "penguin";
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+            spriteName = "girl";
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+            spriteName = "droid";
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Destroy(Instantiate(Resources.Load(spriteName + "_idle"), new Vector3(0, 0, 1), Quaternion.identity), 4);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Destroy(Instantiate(Resources.Load(spriteName + "_happy"), new Vector3(0, 0, 1), Quaternion.identity), 4);
+        }
     }
 }
