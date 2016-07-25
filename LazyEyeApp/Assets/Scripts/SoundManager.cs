0d3a6304f2fc7b12919275bc3bc08a1a6a634aec
Spark11 <patlajanec@gmail.com>
2016-07-25 15:41:00 +0100

    Fixed music and sfx buttons in pause menu

--- LazyEyeApp/Assets/Scripts/SoundManager.cs
+++ LazyEyeApp/Assets/Scripts/SoundManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour {
 
@@ -12,8 +13,10 @@ public class SoundManager : MonoBehaviour {
     private static AudioClip[] sfxClips = { Resources.Load<AudioClip>("sfx_click"), Resources.Load<AudioClip>("sfx_correct"), Resources.Load<AudioClip>("sfx_wrong"), Resources.Load<AudioClip>("sfx_victory"), Resources.Load<AudioClip>("sfx_loss"), };
 
     private bool musicOff;
-    private bool effectsOff;
-    
+    private bool sfxOff;
+
+    private GameObject musicButton;
+    private GameObject sfxButton;
 
     //  this class will be a singleton
     void Awake () {
@@ -25,8 +28,15 @@ public class SoundManager : MonoBehaviour {
         DontDestroyOnLoad(gameObject);
 
         musicOff = PlayerPrefs.GetInt("music") == 1;
-        effectsOff = PlayerPrefs.GetInt("effects") == 1;
+        sfxOff = PlayerPrefs.GetInt("effects") == 1;
         backgroundSource.volume = musicOff ? 0 : 1;
+
+        musicButton = GameObject.Find("musicButton");
+        sfxButton = GameObject.Find("sfxButton");
+
+        musicButton.GetComponent<Image>().sprite = musicOff ? Resources.Load<Sprite>("musicOff") : Resources.Load<Sprite>("musicOn");
+        sfxButton.GetComponent<Image>().sprite = sfxOff ? Resources.Load<Sprite>("sfxOff") : Resources.Load<Sprite>("sfxOn");
+        //GameObject.Find("sfxButton").GetComponent<Image>().sprite = Resources.Load<Sprite>("SoundOff");
     }
 
 
@@ -54,7 +64,7 @@ public class SoundManager : MonoBehaviour {
     //  sfx
     public void PlayEffect(SFX effect)
     {
-        if (!effectsOff)
+        if (!sfxOff)
         {
             sfxSource.clip = sfxClips[(int)effect];
             sfxSource.Play();
@@ -68,12 +78,14 @@ public class SoundManager : MonoBehaviour {
         musicOff = !musicOff;
         PlayerPrefs.SetInt("music", musicOff ? 1 : 0);
         backgroundSource.volume = musicOff ? 0 : 1;
+        musicButton.GetComponent<Image>().sprite = musicOff ? Resources.Load<Sprite>("musicOff") : Resources.Load<Sprite>("musicOn");
     }
 
     public void ToggleEffects()
     {
-        effectsOff = !effectsOff;
-        PlayerPrefs.SetInt("effects", effectsOff ? 1 : 0);
+        sfxOff = !sfxOff;
+        PlayerPrefs.SetInt("effects", sfxOff ? 1 : 0);
+        sfxButton.GetComponent<Image>().sprite = sfxOff ? Resources.Load<Sprite>("sfxOff") : Resources.Load<Sprite>("sfxOn");
     }
 	
 }
